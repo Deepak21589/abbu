869e9779a0b7f8b0e6de8c960fced82ff5a3219d
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-02-28 16:37:07 +0530

    rep.C

---
+++ rep.C
@@ -0,0 +1,18 @@
+#include<string.h>
+int main(void)
+{
+char str[100];
+int i,h,j;
+printf("\nEnter the string fr repeat:");
+scanf("%s",&str[i]);
+printf("\nEnter the num to repeat:");
+scanf("%d",&h);
+//for(i=0;i<=h;i++)
+//{
+for(j=1;j<=h;j++)
+{
+printf("%s",&str[i]);	
+}
+//}	
+return 0;
+}
