3c06eb769bb5dbad2c1372fc87d0faed90d7a45c
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-03-05 02:47:57 -0800

    com.C

---
+++ com.C
@@ -0,0 +1,23 @@
+//run at turbo c
+#include <stdio.h>
+#include<string.h>
+int main(void)                
+{
+	char c[100];
+	int i;
+	puts("Enter the string:");
+	gets(c);
+	for(i=0;i<=strlen(c);i++)
+	{
+	if(c[i]]=='')
+	{
+
+	}
+	else
+	{
+	printf("%s",c);
+	}
+}
+	
+	return 0;
+}
