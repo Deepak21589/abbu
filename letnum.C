f614c4cf3608f6cc0a6de6f377f87c0787b49842
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-03-09 02:42:20 -0800

    letnum.C

---
+++ letnum.C
@@ -0,0 +1,55 @@
+#include <stdio.h>
+ 
+int main(void) 
+{
+int a;
+printf("\nEnter the num:");
+scanf("%d",&a);
+if(a==1)
+{
+	printf("\nOne");
+}
+else if(a==2)
+{
+	printf("\nTwo");
+}
+else if(a==3)
+{
+	printf("\nThree");
+}
+else if(a==4)
+{
+	printf("\nFour");
+}
+else if(a==5)
+{
+	printf("\nFive");
+}
+else if(a==6)
+{
+	printf("\nSix");
+}
+else if(a==7)
+{
+	printf("\nSeven");
+}
+else if(a==8)
+{
+	printf("\nEight");
+}
+else if(a==9)
+{
+	printf("\nNine");
+}
+else if(a==10)
+{
+	printf("\nTen");
+}
+else
+{
+	printf("\nInvalid Data");
+}
+ 
+ 
+	return 0;
+}
